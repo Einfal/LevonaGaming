65df1136e50096ee854143a16f27e19a92e3d34b
Einfal <levona2319@gmail.com>
2025-04-14 10:50:39 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,2 +1,10 @@
 
-// App.js content placeholder - will be filled by assistant shortly
+// Root Levona Site Structure - React + Tailwind
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import './SkullRain.css';
+
+// ... rest of App.js content omitted for brevity, would normally use saved version
+const App = () => <div>Levona Loaded</div>;
+
+export default App;
